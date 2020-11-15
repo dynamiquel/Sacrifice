bad22387c5b9374ac42df2de9e45793f5954af9e
dynamiquel <50085636+dynamiquel@users.noreply.github.com>
2020-11-15 15:24:40 +0000

    Unreal project created

---
+++ Sacrifice/Intermediate/Build/Win64/SacrificeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "S:/Users/Liamk/Development/Games/Sacrifice/Sacrifice/Intermediate/Build/Win64/SacrificeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Sacrifice/Intermediate/Build/Win64/UE4Editor/Inc/Sacrifice/SacrificeGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SACRIFICE_SacrificeGameModeBase_generated_h
+#error "SacrificeGameModeBase.generated.h already included, missing '#pragma once' in SacrificeGameModeBase.h"
+#endif
+#define SACRIFICE_SacrificeGameModeBase_generated_h
+
+#define Sacrifice_Source_Sacrifice_SacrificeGameModeBase_h_15_SPARSE_DATA
+#define Sacrifice_Source_Sacrifice_SacrificeGameModeBase_h_15_RPC_WRAPPERS
+#define Sacrifice_Source_Sacrifice_SacrificeGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define Sacrifice_Source_Sacrifice_SacrificeGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesASacrificeGameModeBase(); \
+	friend struct Z_Construct_UClass_ASacrificeGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ASacrificeGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/Sacrifice"), NO_API) \
+	DECLARE_SERIALIZER(ASacrificeGameModeBase)
+
+
+#define Sacrifice_Source_Sacrifice_SacrificeGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesASacrificeGameModeBase(); \
+	friend struct Z_Construct_UClass_ASacrificeGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ASacrificeGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/Sacrifice"), NO_API) \
+	DECLARE_SERIALIZER(ASacrificeGameModeBase)
+
+
+#define Sacrifice_Source_Sacrifice_SacrificeGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASacrificeGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASacrificeGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASacrificeGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASacrificeGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASacrificeGameModeBase(ASacrificeGameModeBase&&); \
+	NO_API ASacrificeGameModeBase(const ASacrificeGameModeBase&); \
+public:
+
+
+#define Sacrifice_Source_Sacrifice_SacrificeGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASacrificeGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASacrificeGameModeBase(ASacrificeGameModeBase&&); \
+	NO_API ASacrificeGameModeBase(const ASacrificeGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASacrificeGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASacrificeGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASacrificeGameModeBase)
+
+
+#define Sacrifice_Source_Sacrifice_SacrificeGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define Sacrifice_Source_Sacrifice_SacrificeGameModeBase_h_12_PROLOG
+#define Sacrifice_Source_Sacrifice_SacrificeGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Sacrifice_Source_Sacrifice_SacrificeGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Sacrifice_Source_Sacrifice_SacrificeGameModeBase_h_15_SPARSE_DATA \
+	Sacrifice_Source_Sacrifice_SacrificeGameModeBase_h_15_RPC_WRAPPERS \
+	Sacrifice_Source_Sacrifice_SacrificeGameModeBase_h_15_INCLASS \
+	Sacrifice_Source_Sacrifice_SacrificeGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Sacrifice_Source_Sacrifice_SacrificeGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Sacrifice_Source_Sacrifice_SacrificeGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Sacrifice_Source_Sacrifice_SacrificeGameModeBase_h_15_SPARSE_DATA \
+	Sacrifice_Source_Sacrifice_SacrificeGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	Sacrifice_Source_Sacrifice_SacrificeGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	Sacrifice_Source_Sacrifice_SacrificeGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> SACRIFICE_API UClass* StaticClass<class ASacrificeGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Sacrifice_Source_Sacrifice_SacrificeGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
