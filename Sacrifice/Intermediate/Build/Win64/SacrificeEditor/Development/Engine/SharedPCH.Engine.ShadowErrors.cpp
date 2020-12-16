6d1a049987611ed1e5cc5778ee3725216d20b968
dynamiquel <50085636+dynamiquel@users.noreply.github.com>
2020-12-16 16:00:29 +0000

    Migrated project

--- Sacrifice/Intermediate/Build/Win64/SacrificeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,1 +0,0 @@
-#include "S:/Users/Liamk/Development/Games/Sacrifice/Sacrifice/Intermediate/Build/Win64/SacrificeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- Sacrifice/Source/Sacrifice/Sacrifice.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Sacrifice.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Sacrifice, "Sacrifice" );
